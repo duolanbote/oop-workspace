56520a59152a7347e1440291e094ecd710885497
Yuxin Lai <a1893271@emb108w047.ad.adelaide.edu.au>
2025-08-01 16:39:33 +0930

    ovo

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 extern int count(int[], int);
 
 int main(){
-    int test_array[6] = {1,2,3,4,5,6};
+    int test_array[5] = {4,5,6,7,8};
 
     std::cout << "Even:" << count(test_array, 6) << std::endl; 
 }
